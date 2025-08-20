1d5da2e3f4787fb4bfab90369a949aa42ec0566f
basaiyv <115393375+basaiyv@users.noreply.github.com>
2025-08-20 09:19:33 +0800

    Update misc.cpp

--- src/misc.cpp
+++ src/misc.cpp
@@ -127,7 +127,7 @@ class Logger {
 //      Pikafish version
 std::string engine_version_info() {
     std::stringstream ss;
-    ss << "PikafishA " << version << std::setfill('0');
+    ss << "PikafishB " << version << std::setfill('0');
 
     if constexpr (version == "dev")
     {
